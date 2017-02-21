d2d891282c402c49058d83789d2fbccc5e798a05
Harry Yang <harryyang@ttic.edu>
2017-02-20 18:26:05 -0800

    update

--- blend.lua
+++ blend.lua
@@ -1,7 +1,9 @@
 require 'image'
-local source=image.load('')
-local tgt=image.load('')
-local mask=image.load('')
 
-local res=torch.cmul(src,mask)+torch.cmul(tgt,1-mask)
-image.save('',res)
+for i=1,32 do
+    src=image.load(string.format('examples/input_%04d.png',i))
+    tgt=image.load(string.format('examples/demo_real_%04d.pngTOfake_%04d.png',i))
+    mask=image.load（'mask.png')
+	res=torch.cmul(src,mask)+torch.cmul(tgt,1-mask)
+	image.save(string.format('examples/result_%04d.png',i),res)
+end
