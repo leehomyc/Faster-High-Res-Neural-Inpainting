863a3152b8f34f15ecd369251364d4d74ef08709
Harry Yang <harryyang@ttic.edu>
2017-02-20 18:37:00 -0800

    update

--- README.md
+++ README.md
@@ -20,9 +20,10 @@ This is the code for [High-Resolution Image Inpainting using Multi-Scale Neural 
   th run_content_network.lua
   # This will use the trained model to generate the output of the content network
   th run_texture_optimization.lua
-  # This will use the trained model to run texture optimization and generate the final result
+  # This will use the trained model to run texture optimization
+  th blend.lua
+  # This will generate the final result
 ```
-
 
 
 ### Citation
--- run_content_network.lua
+++ run_content_network.lua
@@ -9,6 +9,7 @@ torch.setdefaulttensortype('torch.FloatTensor')
 
 opt = {
   gpu=1,
+  fineSize=128,
   overlapPred=4,
   model_file='models/imagenet_inpaintCenter.t7',
 }
@@ -24,23 +25,26 @@ end
 
 modelG=util.load(opt.model_file,opt.gpu)
 
-local real=torch.Tensor(7,3,512,512)
-local real_ctx = torch.Tensor(7,3,128,128)
+local real=torch.Tensor(32,3,512,512)
+local real_ctx = torch.Tensor(32,3,128,128)
 local fake2 = torch.Tensor(3,256,256)
 local output=torch.Tensor(3,512,512)
 
-for i=1,7 do
-    real[i]=loadImage(string.format('examples/pink_%04d.jpg',i),512)
+for i=1,32 do
+    real[i]=loadImage(string.format('examples/input_%04d.png',i),512)
     real_ctx[i]:copy(image.scale(real[i],128,128))
+    print(real_ctx[i]:max())
+    real_ctx[{i,{1},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred}}] = 2*117/255.0 - 1.0
+    real_ctx[{i,{2},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred}}] = 2*104/255.0 - 1.0
+    real_ctx[{i,{3},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred}}] = 2*123/255.0 - 1.0
 end
 fake = modelG:forward(real_ctx)
-for i=1,7 do
+for i=1,32 do
     fake2:copy(image.scale(fake[i],256,256))
     output:copy(real[i])
     output[{{},{145,368},{145,368}}]:copy(fake2[{{},{17, 240},{17, 240}}])
     output[output:gt(1)]=1
     output[output:lt(-1)]=-1
-    output[output:gt(1)]=1
     output=(output+1)/2
     image.save(string.format('examples/fake_%04d.png',i),output)
 end
