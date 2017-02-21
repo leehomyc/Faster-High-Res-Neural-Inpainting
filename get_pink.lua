370ad5cf86e68a7a449b943e15ec15cff5061532
Harry Yang <harryyang@ttic.edu>
2017-02-20 18:26:40 -0800

    update

--- get_pink.lua
+++ get_pink.lua
@@ -10,7 +10,7 @@ end
 
 for i=1,32 do
     real=loadImage(string.format('examples/real_%04d.png',i),512)
-    real[{{},{166, 345},{166，345}}] = 2*255/255.0 - 1.0
+    real[{{},{166, 345},{166,345}}] = 2*255/255.0 - 1.0
     test=(real+1)/2
     image.save(string.format('examples/input_%04d.png',i),test)
 end
