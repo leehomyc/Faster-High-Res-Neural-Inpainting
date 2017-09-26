9efc3dd82275e19f15527bd18d79a5d82bef2d4d
Harry Yang <harryyang.hk@gmail.com>
2017-09-26 13:08:35 -0700

    Update blend.lua

--- blend.lua
+++ blend.lua
@@ -5,6 +5,6 @@ for i=1,32 do
     tgt=image.load(string.format('examples/demo_input_%04d.pngTOfake_%04d.png.jpg',i,i))
     mask=image.load('mask.png')
     mask=mask[{{1,3},{},{}}]
-	res=torch.cmul(src,mask)+torch.cmul(tgt,1-mask)
-	image.save(string.format('examples/result_%04d.png',i),res)
+    res=torch.cmul(src,mask)+torch.cmul(tgt,1-mask)
+    image.save(string.format('examples/result_%04d.png',i),res)
 end
